a6c42bd58f20406a57f9325ea4c3a4555c97d020
HANSTZ3/HANS-MD <mpyabottzhans@gmail.com>
2024-10-04 12:45:39 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -15205,7 +15205,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "©HANATZ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255750059784",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "255717748823",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
